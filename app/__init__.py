43c3c9dc2f34b6eee50970b0c86ad6cdb4481ae0
Ainsley Escorce-Jones <me@ains.co>
2015-02-15 21:44:12 +0000

    Added heroku support

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,14 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
-app.config.from_pyfile('settings.cfg')
+
+if os.path.exists('settings.cfg'):
+    app.config.from_pyfile('settings.cfg')
+else:
+    app.config.update(
+        IG_CLIENT_ID=os.environ.get('IG_CLIENT_ID'),
+        IG_CLIENT_SECRET=os.environ.get('IG_CLIENT_SECRET')
+    )
 
 from app import views
